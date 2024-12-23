180214dec9878f54ee9e5b93fdf1101e38c4cb1c
Jiten Kumar <77165984+Jiten28@users.noreply.github.com>
2024-12-23 22:36:23 +0530

    finalize the structure of the application

--- Task1(To-do List)/src/App.jsx
+++ Task1(To-do List)/src/App.jsx
@@ -3,7 +3,7 @@ import Todo from './Components/Todo'
 
 const App = () => {
   return (
-    <div className='bg-blue-900 grid py-4 h-screen'>
+    <div className='bg-sky-200 grid py-4 h-screen'>
         <Todo />
     </div>
   )
--- Task1(To-do List)/src/Components/Todo.jsx
+++ Task1(To-do List)/src/Components/Todo.jsx
@@ -3,13 +3,28 @@ import todo_icon from '../assets/todo_icon.png'
 
 const Todo = () => {
   return (
-    <div className='bg-white place-self-center w-11/12 
+    <div className='bg-blue-400 place-self-center w-11/12 
                     max-w-md flex flex-col p-7 
                     min-h-[550px] rounded-lg shadow-lg'>
-        
+
+        {/* title and icon */}
         <div className='flex items-center mt-7 gap-2'>
             <img className='w-8' src={todo_icon} alt="" />
             <h1 className='text-3xl font-semibold'>To-do List</h1>
+        </div>
+
+        {/* inputbox */}
+        <div className='flex itmes-center my-7 bg-blue-200 rounded-full'>
+            <input type='text' className=' border-0 outline-none flex-1 
+            bg-transparent h-14 pl-6 pr-2 placeholder:text-slate-600' 
+            placeholder='Add a task' />
+            <button className='border-none rounded-full bg-orange-700 w-32
+            h-14 text-white font-medium cursor-pointer'>Add +</button>
+        </div>
+
+        {/* tasks */}
+        <div className='flex flex-col gap-4'>
+            
         </div>
     </div>
   )
